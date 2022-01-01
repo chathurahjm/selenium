08cd6b5e4710bc86adae0f748b4a8433756ca680
chathuraj <chathudagr8@yahoo.com>
2022-01-01 14:49:59 +0530

    first 1 2

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -12,6 +12,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,90 +30,175 @@ public class AppTest extends base
     String avg,count,collection,document,max,min,updateTime;
     ArrayList<String> list = new ArrayList<String>();
     @Test
-    public void shouldAnswerWithTrue() throws InterruptedException {
+    public void honda() throws InterruptedException {
 
 
-        String[] yearList = {"2014","2015","2016","2017","2018","2019","2020","2021"};
 
-        for (String year:yearList){
-            OpenChrom();
+        String[] VehicleList = {"honda_honda-vezel","honda_honda-grace","honda_honda-civic","honda_honda-crv"};
+        for (String vehicleModel :VehicleList) {
 
 
-        for (int i = 1 ; i<8;i++) {
+            String[] yearList = {"2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"};
+            for (String year : yearList) {
+                OpenChrom();
 
-            driver.get("https://ikman.lk/en/ads/sri-lanka/cars/honda/vezel?sort=date&order=desc&buy_now=0&urgent=0&page="+i+"&numeric.model_year.minimum="+year+"&numeric.model_year.maximum="+year+"&tree.brand=honda_honda-vezel");
-            String noresults = "";
-            try {
-                noresults = driver.findElement(By.className("no-result-text--16bWr")).getText();
-            }
-            catch (Exception e)
-            {
-                
-            }
-                
-                if(noresults.contains("No results found"))
-                {
-                    break;
+
+                for (int i = 1; i < 8; i++) {
+
+                    driver.get("https://ikman.lk/en/ads/sri-lanka/cars/honda/?sort=date&order=desc&buy_now=0&urgent=0&page=" + i + "&numeric.model_year.minimum=" + year + "&numeric.model_year.maximum=" + year + "&tree.brand="+vehicleModel);
+                    String noresults = "";
+                    try {
+                        noresults = driver.findElement(By.className("no-result-text--16bWr")).getText();
+                    } catch (Exception e) {
+
+                    }
+
+                    if (noresults.contains("No results found")) {
+                        break;
+
+                    } else {
+                        try {
+                            List<WebElement> elm = driver.findElements(By.xpath("//*[contains(@class,\"normal--2QYVk gtm-normal-ad\")]//*/span[1]"));
+                            for (WebElement price : elm) {
+                                String _price = price.getText();
+                                _price = _price.replace("Rs", "");
+                                _price = _price.replace(",", "");
+                                _price = _price.replace(" ", "");
+                                list.add(_price);
+                            }
+                        } catch (Exception e) {
+
+                        }
+                    }
+
 
                 }
-                
+
+                driver.quit();
 
 
-            else {
+                ArrayList<Integer> Intlist = new ArrayList<Integer>();
+                for (String price : list) {
+
+                    int intPrice = Integer.parseInt(price);
+                    Intlist.add(intPrice);
+                }
+
+
                 try {
-                    List<WebElement> elm = driver.findElements(By.xpath("//*[contains(@class,\"normal--2QYVk gtm-normal-ad\")]//*/span[1]"));
-                    for (WebElement price : elm) {
-                        String _price = price.getText();
-                        _price = _price.replace("Rs", "");
-                        _price = _price.replace(",", "");
-                        _price = _price.replace(" ", "");
-                        list.add(_price);
-                    }
+                    count = Integer.toString(list.size());
+                    collection = vehicleModel;
+                    document = year;
+                    min = Integer.toString(Intlist.stream().collect(Collectors.minBy(Comparator.naturalOrder())).get());
+                    max = Integer.toString(Intlist.stream().collect(Collectors.maxBy(Comparator.naturalOrder())).get());
+                    avg = getAvg(Intlist);
+                    updateTime = LocalDate.now().toString();
                 } catch (Exception e) {
 
                 }
-            }
-            
-
-           
-        }
-
-        driver.quit();
 
 
-        ArrayList<Integer> Intlist = new ArrayList<Integer>();
-        for (String price:list) {
-
-            int intPrice = Integer.parseInt(price);
-            Intlist.add(intPrice);
-        }
+                list.clear();
+                Intlist.clear();
 
 
-        try {
-            count = Integer.toString(list.size());
-            collection="Vezel";
-            document = year;
-            min = Integer.toString(Intlist.stream().collect(Collectors.minBy(Comparator.naturalOrder())).get());
-            max = Integer.toString(Intlist.stream().collect(Collectors.maxBy(Comparator.naturalOrder())).get());
-            avg = getAvg(Intlist);
-            updateTime= LocalDate.now().toString();
-        }
-        catch (Exception e)
-        {
-
-        }
-
-
-        list.clear();
-        Intlist.clear();
-
-
-
-            updateDB(collection,document,avg,min,max,count,updateTime);
-
+                if(avg!="") {
+                    updateDB(collection, document, avg, min, max, count, updateTime);
+                }
 
             }
         }
+        }
+
+    @Test
+    public void toyota() throws InterruptedException {
+
+
+
+        String[] VehicleList = {"toyota_toyota-vitz","toyota_toyota-axio","toyota_toyota-premio","toyota_toyota-aqua","toyota_toyota-corolla","toyota_toyota-prius","toyota_toyota-land-cruiser-prado","toyota_toyota-allion","toyota_toyota-ch-r"};
+        for (String vehicleModel :VehicleList) {
+
+
+            String[] yearList = {"2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013","2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"};
+            for (String year : yearList) {
+                OpenChrom();
+
+
+                for (int i = 1; i < 8; i++) {
+
+                    driver.get("https://ikman.lk/en/ads/sri-lanka/cars/toyota/?sort=date&order=desc&buy_now=0&urgent=0&page=" + i + "&numeric.model_year.minimum=" + year + "&numeric.model_year.maximum=" + year + "&tree.brand="+vehicleModel);
+                    String noresults = "";
+                    try {
+                        noresults = driver.findElement(By.className("no-result-text--16bWr")).getText();
+                    } catch (Exception e) {
+
+                    }
+
+                    if (noresults.contains("No results found")) {
+                        break;
+
+                    } else {
+                        try {
+                            List<WebElement> elm = driver.findElements(By.xpath("//*[contains(@class,\"normal--2QYVk gtm-normal-ad\")]//*/span[1]"));
+                            for (WebElement price : elm) {
+                                String _price = price.getText();
+                                if (_price.equals("AUTH AGENT"))
+                                {
+                                    _price=null;
+                                }
+                                _price = _price.replace("Rs", "");
+                                _price = _price.replace(",", "");
+                                _price = _price.replace(" ", "");
+
+                                list.add(_price);
+                                list.removeAll(Collections.singleton(null));
+
+
+                            }
+                        } catch (Exception e) {
+
+                        }
+                    }
+
+
+                }
+
+                driver.quit();
+
+
+                ArrayList<Integer> Intlist = new ArrayList<Integer>();
+                for (String price : list) {
+
+
+
+                    int intPrice = Integer.parseInt(price);
+                    Intlist.add(intPrice);
+                }
+
+
+                try {
+                    count = Integer.toString(list.size());
+                    collection = vehicleModel;
+                    document = year;
+                    min = Integer.toString(Intlist.stream().collect(Collectors.minBy(Comparator.naturalOrder())).get());
+                    max = Integer.toString(Intlist.stream().collect(Collectors.maxBy(Comparator.naturalOrder())).get());
+                    avg = getAvg(Intlist);
+                    updateTime = LocalDate.now().toString();
+                } catch (Exception e) {
+
+                }
+
+
+                list.clear();
+                Intlist.clear();
+
+                if(avg!="") {
+                    updateDB(collection, document, avg, min, max, count, updateTime);
+                }
+
+            }
+        }
+    }
 
 
 
